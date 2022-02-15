a2984e893b2aa160a8167a48962bd0df797c8313
vaibhavdata <vaibhavmc01@gmail.com>
2022-02-15 11:22:49 +0530

    allowed host

--- plants_dekho/settings.py
+++ plants_dekho/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-r^y)v34ih0-$&gu1n7$%$2kw=&il!z!p$6v0**4*p2hadx$297
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+   '*',
+   'https://plantdekho.herokuapp.com/',
+   '127.0.0.1',
+]
 
 
 # Application definition
